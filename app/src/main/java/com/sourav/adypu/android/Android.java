1c43e227b5353948561e034c3c190c57d8a9f3bc
adypu <sourav.nayak@adypu.edu.in>
2020-02-18 12:53:10 +0530

    sourav

---
+++ app/src/main/java/com/sourav/adypu/android/Android.java
@@ -0,0 +1,13 @@
+package com.sourav.adypu.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Android extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android);
+    }
+}
